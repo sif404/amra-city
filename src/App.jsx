7fd92c075a4f2ec88b02a3758c73ed5a00c01b27
sif404 <troys8926@gmail.com>
2026-04-24 01:52:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import Hero from "./component/Hero";
+import Sections from "./component/Sections";
+import Navbar from "./component/Navbar";
 
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Sections />
+    </>
+  );
+}
